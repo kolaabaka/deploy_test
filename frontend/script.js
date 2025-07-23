45e49030a6d23fd9da3b6db22850f20993a647db
KolaBaka <kolamba040301@gmail.com>
2025-07-23 16:01:07 +0300

    delete excess repository, modified JS script

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 async function fetchHello() {
-    const response = await fetch('http://localhost:80/api/hello',{
+    const response = await fetch('/api/hello',{
 		mode: 'no-cors'
 	});
     const text = await response.text();
